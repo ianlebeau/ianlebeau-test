6d0e269d55e7dd3446df9e639f774e039c65f879
Ian LeBeau <ianlebeau@gmail.com>
2015-05-03 11:40:13 -0700

    Test index page

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<title>Swimming Pool Skimmers | Solar-Breeze Robotic Floating Pool Skimmer</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="Description" content="The Solar-Breeze pool skimmer uses energy from the sun to keep your pool clean by removing leaves, debris, pollens, dust and suntan oils from the surface of your pool." />
+<meta name="Author" content="Metro Studios - www.metro-studios.com" />
+<meta name="google-site-verification" content="GdX3QI7DgpzIXXd6NxmsvJjYupQP7R0doM9hyUV7WDo" />
+<meta name="msvalidate.01" content="DCB5705C8939F3425A2A6E2A3B75BD97" />
+<meta name="y_key" content="690fed563f89c745" />
+<link href="/favicon.ico" rel="shortcut icon" />
+<style type="text/css" media="screen, projection">
+<!--
+@import url("/css/solar-style.css");
+-->
+</style>
+<script src="/scripts/jquery-1.4.2.min.js" type="text/javascript"></script>
+<script src="/scripts/jquery-ui.min.js" type="text/javascript"></script>
+<script src="/scripts/cufon-yui.js" type="text/javascript"></script>
+<script src="/scripts/gillsansstd.font.js" type="text/javascript"></script>
+<script src="/scripts/swfobject.js" type="text/javascript"></script>
+<link href="//vjs.zencdn.net/c/video-js.css" rel="stylesheet">
+
+<!--<script type="text/javascript">
+	swfobject.embedSWF("/flash/loop.swf", "ban-right", "490", "270", "8.0", "/flash/expressInstall.swf", {}, {menu:"false",wmode:"transparent"}, {});
+</script>-->
+</head>
+
+<body>
+<div id="navigation" class="clearfix"><div class="gutter">
+<? require_once($_SERVER['DOCUMENT_ROOT'].'/include/navigation.php'); ?>
+</div></div>
+
+<div id="wrapper">
+<div id="logo" class="clearfix">
+<? require_once($_SERVER['DOCUMENT_ROOT'].'/include/header.php'); ?>
+</div>
+<div id="banner" class="clearfix">
+	<div id="ban-left"><div class="gutter">
+		<h3 class="bannertextreplace"><span>Clean your pool for free with energy from the sun</span></h3>
+		<p>The Solar-Breeze pool skimmer is an intelligent robot that uses free energy from the sun to keep your swimming pool clean by removing leaves, debris, organic material, pollens, dust and even suntan oils from the surface of your pool before they sink to the bottom.  Click on the links to learn more.</p>
+		<div class="purchase"><a href="/cgi-bin/commerce.cgi?preadd=action&amp;key=SPT1002"><img src="/images/btn-purchase.gif" border="0" /></a></div>	
+        <!--/cgi-bin/commerce.cgi?preadd=action&amp;key=SPT1002 location for cart when there is more stock-->
+	</div></div>
+    <div id="ban-right">
+        
+	    <video id="video" class="video-js vjs-default-skin" width="470" height="265" controls data-setup='{"autoplay":true, "preload":"auto", "loop":true}'>
+            <source type="video/mp4" src="/flash/loop.mp4">
+            <source type="video/ogg" src="/flash/loop.ogg">
+            
+        </video>
+        <div id="border"></div>
+        <div id="text-overlay"><h4><a href="/automatic-pool-skimmer/how-it-works/">FIND OUT HOW IT WORKS</a></h4></div>
+    </div>
+</div>
+
+<div id="index">
+	<div class="col">
+		<h2><a href="/automatic-pool-skimmer/save-time/">Save Time</a></h2>
+	  <p>With the Solar-Breeze your swimming pool is always "swim ready".  No more hand skimming to remove debris before you jump in and no more rushing to clean the pool before your party guests arrive!</p>
+		<a href="/automatic-pool-skimmer/save-time/"><img src="/images/btn-learn.jpg" alt="Learn More" border="0" /></a>
+	</div>
+	<div class="col">
+		<h2><a href="/automatic-pool-skimmer/save-energy/">Save Energy and Money</a></h2>
+		<p>Removing debris from the surface before it sinks to the bottom means that less bottom cleaning and filtration is required.  Find out how Solar-Breeze owners are reducing their pool pump usage and lowering their energy bills!</p>
+		<a href="/automatic-pool-skimmer/save-energy/"><img src="/images/btn-learn.jpg" alt="Learn More" border="0" /></a>
+	</div>
+	<div class="col">
+		<h2>Holiday Discount Coupon</h2>
+		<p>Enter your email below and join our mailing list to receive your Special Promo Code.</p>
+		<table border="0" cellspacing="0" cellpadding="3" bgcolor="#F1A782" style="border:2px solid #FFFFFF;margin-top:-50px;">
+<tbody><tr>
+<td align="center" style="font-weight: bold; font-family:Arial; font-size:12px; color:#FFFFFF;">Join Our Mailing List</td>
+</tr>
+<tr>
+<td align="center" style="border-top:2px solid #FFFFFF">
+<form name="ccoptin" action="http://visitor.r20.constantcontact.com/d.jsp" target="_blank" method="post" style="margin-bottom:2;">
+<input type="hidden" name="llr" value="dmy9yceab">
+<input type="hidden" name="m" value="1103899746691">
+<input type="hidden" name="p" value="oi">
+<font style="font-weight: normal; font-family:Arial; font-size:12px; color:#FFFFFF;">Email:</font> <input type="text" name="ea" size="20" value="" style="font-size:10pt; border:1px solid #999999;">
+<input type="submit" name="go" value="Go" class="submit" style="font-family:Verdana,Geneva,Arial,Helvetica,sans-serif; font-size:10pt;">
+</form>
+</td>
+</tr>
+</tbody></table>
+		</div>
+	<br style="clear:both;" />
+</div>
+
+</div>
+
+<div id="footer"><div class="gutter">
+<? require_once($_SERVER['DOCUMENT_ROOT'].'/include/footer.php'); ?>
+</div></div>
+
+<script type="text/javascript">
+	Cufon.replace('h2');
+	Cufon.replace('#nav li', {hover: true});
+    Cufon.replace('#text-overlay h4');
+</script>
+<script src="//vjs.zencdn.net/c/video.js"></script>
+<script type="text/javascript">
+$(window).load(function(){
+    var myPlayer = _V_("video"); 
+    // EXAMPLE: Start playing the video. 
+    myPlayer.play();
+});
+</script>
+<script type="text/javascript"> Cufon.now(); </script>
+</body>
+<script src="http://i.simpli.fi/dpx.js?cid=25&action=100&segment=2999706&m=1"></script>
+</html>
